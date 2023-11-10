dbbe6f6a7c8092a3ad2712834612958bc8a3f594
Roopra <c3011405@hallam.shu.ac.uk>
2023-11-10 10:48:16 +0000

    onclick for js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 (function(){
     // code starts here
     document.getElementById("myHeading").innerHTML = "harpreet";
+    document.querySelector("nav ul li").setAttribute("class", "currentPage");
+    document.getElementById("myTestEvent").onclick = function(){
+        console.info("hi");
+    }
 
     // code ends here
 })(); 
